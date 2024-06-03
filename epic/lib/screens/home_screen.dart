ff6ebed3cdec44a29a3ce499becdd1de660771db
JohnNixon6972 <johnbrightnixon@gmail.com>
2024-06-03 16:01:29 +0100

    firebase works perfectly

--- epic/lib/screens/home_screen.dart
+++ epic/lib/screens/home_screen.dart
@@ -7,8 +7,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return const Scaffold(
-    
       body: Center(
         child: Text('Home Screen'),
       ),
