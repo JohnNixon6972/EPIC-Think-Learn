07df3d9b7f79ad0dda1906e08dd0c6c11945f8b0
JohnNixon6972 <johnbrightnixon@gmail.com>
2024-06-03 15:41:13 +0100

    updated git ignore

--- epic/lib/main.dart
+++ epic/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home:
-             MaterialApp.router(routerConfig: Routes.getRouter));
+        home: MaterialApp.router(
+            debugShowCheckedModeBanner: false, routerConfig: Routes.getRouter));
   }
 }
