c278cebf80ae2c68195bcce44255295fd0c55786
JohnNixon6972 <johnbrightnixon@gmail.com>
2024-07-04 12:21:39 +0100

    updated Strategy model to include user stats

--- epic/lib/features/strategies/model/strategy_model.dart
+++ epic/lib/features/strategies/model/strategy_model.dart
@@ -4,20 +4,31 @@ class StrategyModel {
   final Strategies strategy;
   int level;
   int days;
+  int averageTime;
+  int averageAccuracy;
+  DateTime lastPlayed;
 
   StrategyModel({
     required this.strategy,
     required this.level,
     required this.days,
+    required this.averageTime,
+    required this.averageAccuracy,
+    required this.lastPlayed,
   });
 
   factory StrategyModel.fromMap(Map<String, dynamic> map) {
     final strategy = Strategies.values
         .firstWhere((element) => element.name == map['strategy']);
+
     return StrategyModel(
+      averageTime: map['averageTime'],
+      averageAccuracy: map['averageAccuracy'],
       strategy: strategy,
       level: map['levels'],
       days: map['days'],
+      lastPlayed: DateTime.fromMillisecondsSinceEpoch(
+          map['lastPlayed'].millisecondsSinceEpoch),
     );
   }
 
@@ -26,6 +37,9 @@ class StrategyModel {
       'strategy': strategy.name,
       'levels': level,
       'days': days,
+      'averageTime': averageTime,
+      'averageAccuracy': averageAccuracy,
+      'lastPlayed': lastPlayed,
     };
   }
 }
