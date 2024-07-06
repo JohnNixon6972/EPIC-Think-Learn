398b8ee663dde9f10b22ff6e461811296ece175b
Hrutuja Patnekar <hrutujapatnekar22@gmail.com>
2024-07-06 15:49:21 +0100

    updated the game for self regulataion

--- epic/lib/cores/games/how_long_is_a_minute/controllers/gameController.dart
+++ epic/lib/cores/games/how_long_is_a_minute/controllers/gameController.dart
@@ -1,3 +1,4 @@
+import 'package:epic/cores/games/how_long_is_a_minute/controllers/gameState.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:async';
 
@@ -6,33 +7,48 @@ final gameControllerProvider =
   return GameController();
 });
 
-class GameState {
-  final bool isRunning;
-  final int elapsedTime; // in milliseconds
-
-  GameState({required this.isRunning, required this.elapsedTime});
-}
-
 class GameController extends StateNotifier<GameState> {
-  GameController() : super(GameState(isRunning: false, elapsedTime: 0));
+  GameController()
+      : super(GameState(isRunning: false, elapsedTime: 0, points: 0, ));
   Timer? _timer;
 
   void startTimer() {
     if (state.isRunning) return;
 
-    state = GameState(isRunning: true, elapsedTime: 0);
+    state = GameState(isRunning: true, elapsedTime: 0, points: state.points);
     _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
-      state = GameState(isRunning: true, elapsedTime: state.elapsedTime + 100);
+      state = GameState(
+          isRunning: true,
+          elapsedTime: state.elapsedTime + 100,
+          points: state.points);
     });
   }
 
   void stopTimer() {
     _timer?.cancel();
-    state = GameState(isRunning: false, elapsedTime: state.elapsedTime);
+    state = GameState(
+        isRunning: false, elapsedTime: state.elapsedTime, points: state.points);
   }
 
   void resetTimer() {
     _timer?.cancel();
-    state = GameState(isRunning: false, elapsedTime: 0);
+    state = GameState(isRunning: false, elapsedTime: 0, points: state.points);
   }
+
+  void updatePoints() {
+    int guess = state.elapsedTime;
+    int points = 0;
+    if (guess == 60000) {
+      points = 20;
+    } else if (guess <= 70000 && guess >= 50000) {
+      points = 10; // Close guess
+    } else if (guess < 50000 || guess > 70000) {
+      points = 5; // Too fast
+    }
+
+    state = state.copyWith(points: state.points + points);
+    print(points);
+  }
+
+  int get points => state.points;
 }
